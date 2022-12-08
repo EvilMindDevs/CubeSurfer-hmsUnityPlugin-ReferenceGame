5fafcbed1649af9e678dc9c162c4dd8f59be28f5
Andronovo-bit <seyyid364@gmail.com>
2022-12-08 10:59:05 +0300

    Edited push kit and added new functions for token operation.

--- Assets/_Scripts/Integration/HMS/PushKİt/PushKitManager.cs
+++ Assets/_Scripts/Integration/HMS/PushKİt/PushKitManager.cs
@@ -1,12 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using HuaweiMobileServices.Push;
 using HuaweiMobileServices.Utils;
 using HmsPlugin;
+using UnityEngine.UI;
 
 public class PushKitManager : Singleton<PushKitManager>
 {
+    private string pushToken;
+    private Text remoteMessageText, tokenText;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +22,14 @@ public class PushKitManager : Singleton<PushKitManager>
         if (HMSPushKitManager.Instance != null)
         {
             CancelInvoke("CheckHMSPushKitManager");
+            HMSPushKitManager.Instance.OnTokenSuccess = OnNewToken;
+            HMSPushKitManager.Instance.OnTokenFailure = OnTokenError;
+            HMSPushKitManager.Instance.OnTokenBundleSuccess = OnNewToken;
+            HMSPushKitManager.Instance.OnTokenBundleFailure = OnTokenError;
+            HMSPushKitManager.Instance.OnMessageSentSuccess = OnMessageSent;
+            HMSPushKitManager.Instance.OnSendFailure = OnSendError;
+            HMSPushKitManager.Instance.OnMessageDeliveredSuccess = OnMessageDelivered;
+            HMSPushKitManager.Instance.OnMessageReceivedSuccess = OnMessageReceived;
             HMSPushKitManager.Instance.OnNotificationMessage = OnNotificationMessage;
             HMSPushKitManager.Instance.NotificationMessageOnStart = NotificationMessageOnStart;
             HMSPushKitManager.Instance.Init();
@@ -28,7 +38,7 @@ public class PushKitManager : Singleton<PushKitManager>
         }
     }
 
-        private void OnNotificationMessage(NotificationData data)
+    private void OnNotificationMessage(NotificationData data)
     {
         Debug.Log("[HMSPushDemo] CmdType: " + data.CmdType);
         Debug.Log("[HMSPushDemo] MsgId: " + data.MsgId);
@@ -45,5 +55,62 @@ public class PushKitManager : Singleton<PushKitManager>
         /* TODO: Make your own logic here
          * notificationDataOnStart = data;
          */
+    }
+
+    public void OnNewToken(string token)
+    {
+        Debug.Log($"[HMS] Push token from OnNewToken is {token}");
+        if (token != "")
+        {
+            pushToken = token;
+            tokenText.text = "Push Token: " + pushToken;
+        }
+    }
+
+    public void OnTokenError(Exception e)
+    {
+        Debug.Log("Error asking for Push token");
+        Debug.Log(e.StackTrace);
+    }
+
+    public void OnMessageReceived(RemoteMessage remoteMessage)
+    {
+        var id = remoteMessage.MessageId;
+        var from = remoteMessage.From;
+        var to = remoteMessage.To;
+        var data = remoteMessage.Data;
+        remoteMessageText.text = $"ID: {id}\nFrom: {from}\nTo: {to}\nData: {data}";
+    }
+
+    public void OnNewToken(string token, Bundle bundle)
+    {
+        Debug.Log($"[HMS] Push token from OnNewToken is {token}");
+        if (token != "")
+        {
+            pushToken = token;
+            tokenText.text = "Push Token: " + pushToken;
+        }
+    }
+
+    public void OnTokenError(Exception exception, Bundle bundle)
+    {
+        Debug.Log("Error asking for Push token");
+        Debug.Log(exception.StackTrace);
+    }
+
+    public void OnMessageSent(string msgId)
+    {
+        Debug.Log(msgId);
+    }
+
+    public void OnMessageDelivered(string msgId, Exception exception)
+    {
+        Debug.Log("Message Delivered");
+        Debug.Log(exception.StackTrace + " , Message ID:" + msgId);
+    }
+
+    public void OnSendError(string msgId, Exception exception)
+    {
+        Debug.Log(exception.StackTrace + " , Message ID:" + msgId);
     }
 }
